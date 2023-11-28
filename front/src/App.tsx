a8313284a6a342eed638be14d450db9f2ffb2572
KazumaEnomoto <kk.yzgb1310@gmail.com>
2023-11-28 22:50:51 +0900

    ヘッダーの作成

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import UserHeader from './components/UserHeader';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <UserHeader />
       <p>Hello React</p>
     </div>
   )
---
+++ front/src/components/UserHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserHeader: React.FC = () => {
+    return (
+        <div className="UserHeader">
+            <p>ヘッダー</p>
+        </div>
+    )
+}
+
+export default UserHeader;
